9ae2a97bd29dddeebb19acc18f50951a66b16577
zaskiaramadhan <ramadhanizaskia753@gmail.com>
2025-10-24 11:37:30 +0700

    Initial commit

---
+++ src/HelloWorld.java
@@ -0,0 +1,12 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, Git!");
+        System.out.println("Ini adalah proyek Java sederhana");
+
+        String kalimat1 = "saya bapak budi";
+        String kalimat2 = "saya ibu budi";
+        System.out.println("Berikut : " + kalimat1 + " " + kalimat2);
+
+    }
+
+}
