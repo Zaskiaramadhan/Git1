51baf36c55111d305b411e72e8fe24b8ca5d8489
zaskiaramadhan <ramadhanizaskia753@gmail.com>
2025-10-24 12:28:25 +0700

    huhu

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -2,6 +2,7 @@ public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello, Git!");
         System.out.println("Ini adalah proyek Java sederhana");
+        System.out.println("Zakia");
 
         String kalimat1 = "saya bapak budi";
         String kalimat2 = "saya ibu budi";
